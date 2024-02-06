8db5b2908b1f79915d8ef9f0967ca3ad1e448e63
AvaCanning <ava.canning@ucdconnect.ie>
2024-02-06 14:43:17 +0000

    draw grid function

--- Main.java
+++ Main.java
@@ -30,33 +30,36 @@ public class Main extends PApplet{
         strokeWeight(2);
 
 
+        drawGrid(400, 100, 30);
 
-        // Draw one hexagon
+    }
 
+
+    public void drawHexagon(int xPos, int yPos, int sideLength) { // Give coord of top of left vertical line
+
+        line(xPos, yPos, xPos, yPos+sideLength); // Left line
+        line(xPos+(sideLength*2), yPos, xPos+(2*sideLength), yPos+sideLength); // Right line
+        line(xPos, yPos, xPos+sideLength, yPos-(sideLength/2)); // Top left line
+        line(xPos+sideLength, yPos-(sideLength/2), xPos+(2*sideLength), yPos); // Top right line
+        line(xPos, yPos+sideLength, xPos+sideLength, yPos+(sideLength +  (sideLength/2))); // Bottom left line
+        line(xPos+sideLength, yPos+(sideLength +  (sideLength/2)), xPos+(sideLength*2), yPos+sideLength); // Bottom right line
+    }
+
+
+
+    public void drawGrid(int xPos, int yPos, int sideLength) { // xPos of top left of left line of first row
         for (int i = 0; i < 5; i++) {
             for (int j = 0; j < 5+i; j++) {
-                drawHexagon(250 - (40*j) + (20*i), 100 + (30*i));
+                drawHexagon(xPos - ((sideLength*2)*j) + (sideLength*i), yPos + ((sideLength + (sideLength/2))*i), sideLength);
             }
         }
 
         for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 8-i; j++) {
-                drawHexagon(30 + (40*j) + (20*i), 250 + (30*i));
+                drawHexagon(xPos-(11*sideLength) + ((sideLength*2)*j) + (sideLength*i), yPos+(7*sideLength + (sideLength/2)) + ((sideLength + (sideLength/2))*i), sideLength);
             }
         }
 
-
-    }
-
-
-    public void drawHexagon(int xPos, int yPos) { // Give coord of top of left vertical line
-
-        line(xPos, yPos, xPos, yPos+20); // Left line
-        line(xPos+40, yPos, xPos+40, yPos+20); // Right line
-        line(xPos, yPos, xPos+20, yPos-10); // Top left line
-        line(xPos+20, yPos-10, xPos+40, yPos); // Top right line
-        line(xPos, yPos+20, xPos+20, yPos+30); // Bottom left line
-        line(xPos+20, yPos+30, xPos+40, yPos+20); // Bottom right line
     }
 
 }
