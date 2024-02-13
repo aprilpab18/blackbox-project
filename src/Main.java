d7a99354b3adf962cffcc13d2ca45222fe00fb14
Amy Tully <amy.tully@ucdconnect.ie>
2024-02-13 11:55:42 +0000

    push atom placement branch

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import main.java.ui.Board;
+import processing.core.PApplet;
+
+public class Main extends PApplet {
+    public static void main(String[] args) {
+
+        Board board = new Board();
+
+        PApplet.main("board", args);
+
+
+
+
+    }
+}
