57f157eb16480e9084ec3e96e7c111fb8cdec576
April Pabalan <april.pabalan@ucdconnect.ie>
2024-03-28 13:37:50 +0000

    Error Handling: Shows one after the other instead
    Signed-off-by: April Pabalan <april.pabalan@ucdconnect.ie>

--- Main.java
+++ Main.java
@@ -110,7 +110,7 @@ public class Main extends PApplet {
                 text("Number not in range, please try again.", 350, 625);
             }
 
-            if(duplicateInput){
+            else if(duplicateInput){
                 fill(255, 0, 0);
                 textSize(20);
                 text("Duplicate input number, please try again.", 350, 625);
